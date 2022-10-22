b53ffe736e601f3d1a738f329e96d5aed4c9c40f
DanielOlsavsky <olsavskyd@spse-po.sk>
2022-10-22 12:17:56 +0200

    Spojenie s firebasom

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,20 @@ import 'package:blood_app/home.dart';
 import 'package:flutter/material.dart';
 import 'package:blood_app/loading.dart';
 import 'package:blood_app/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: "/",
-  routes:{
-    "/":(context) => Login(),
-    "/home": (context) => Home(),
-  },
-  theme: ThemeData(
-    primarySwatch: Colors.red,
-  ),
-));
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    initialRoute: "/",
+    routes:{
+      "/":(context) => Login(),
+      "/home": (context) => Home(),
+    },
+    theme: ThemeData(
+      primarySwatch: Colors.red,
+    ),
+  ));
+}
 
