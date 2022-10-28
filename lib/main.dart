13b1c4a72ef5b598d4d391ae3e5957726f226550
MatejTomko <mtomko2004@gmail.com>
2022-10-28 21:21:11 +0200

    Pridane prihlasenie/registracia + firebase prepojenie fix

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,22 @@ import 'package:blood_app/loading.dart';
 import 'package:blood_app/login.dart';
 import 'package:blood_app/first.dart';
 import 'package:blood_app/registration.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: "/",
-  routes:{
-
-    "/":(context) => First(),
-    "/home": (context) => Home(),
-    "/login": (context) => Login(),
-    "/registration": (context) => Registration(),
-  },
-  theme: ThemeData(
-    primarySwatch: Colors.red,
-  ),
-));
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    initialRoute: "/",
+    routes:{
+      "/":(context) => First(),
+      "/home": (context) => Home(),
+      "/login": (context) => Login(),
+      "/registration": (context) => Registration(),
+    },
+    theme: ThemeData(
+      primarySwatch: Colors.red,
+    ),
+  ));
+}
 
