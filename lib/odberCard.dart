8bc10190920fdb6a811ebc3eca9fcb76563ffa1a
DanielOlsavsky <olsavskyd@spse-po.sk>
2022-11-05 16:23:47 +0100

    BottomBar,zobrazovanie odberov

---
+++ lib/odberCard.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'odber.dart';
+
+class OdberCard extends StatelessWidget {
+  final odber _odber;
+
+  OdberCard(this._odber);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Text("Mnozstvo: ${_odber.mnozstvo}"),
+                  )
+                ],
+              ),
+              Row(children: [
+                Text("Datum: ${_odber.datum}"),
+              ],)
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
